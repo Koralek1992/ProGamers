c27319e12f22469ab293482d630e62b522851d95
Koralek <dkoralek2@gmail.com>
2022-09-15 11:45:04 +0200

    skończona sekcja o nas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 const nav = document.querySelector('.navbar')
+const allNavItems = document.querySelectorAll('.nav-link')
+const navList = document.querySelector('.navbar-collapse')
 function addShadow() {
     if (window.scrollY >= 100) {
         nav.classList.add('shadow-bg')
@@ -7,5 +9,9 @@ function addShadow() {
         nav.classList.remove('shadow-bg')
     }
 }
+
+allNavItems.forEach(item => item.addEventListener('click', () => navList.remove('show')))
+
 window.addEventListener('scroll', addShadow)
 })
+
